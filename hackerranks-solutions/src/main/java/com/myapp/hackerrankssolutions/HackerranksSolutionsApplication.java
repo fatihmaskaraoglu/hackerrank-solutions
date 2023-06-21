fc60455ec81cabcb0dde363799e685c4c3d0071d
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-06-22 00:33:51 +0300

    Init Hackerrank Solutions

---
+++ hackerranks-solutions/src/main/java/com/myapp/hackerrankssolutions/HackerranksSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.hackerrankssolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerranksSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerranksSolutionsApplication.class, args);
+	}
+
+}
