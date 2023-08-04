cbb4d64c41b351768b5358cbfaa822c4a0b8783f
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-08-04 22:01:41 +0300

    threemonthspreparationkit solution week 2 commits

---
+++ hackerranks-solutions/src/main/java/com/myapp/hackerrankssolutions/threemonthspreparationkit/week2/flippingbits/Solution.java
@@ -0,0 +1,45 @@
+package com.myapp.hackerrankssolutions.threemonthspreparationkit.week2.flippingbits;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        // Write your code here
+        return 4294967295L - n;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
